f2d8cab7752a23ac355b1d5b917be860b6c68a83
Ida Skogvoll <idaskogvoll@Idas-MacBook-Pro-2.local>
2019-01-08 16:37:34 +0100

    Forandret output til noe annet

--- std_lib_facilities_mac/main.cpp
+++ std_lib_facilities_mac/main.cpp
@@ -2,7 +2,7 @@
 #include "std_lib_facilities.h"
 
 int main(){
-    cout << "The rather old \"Hello, World!\" message\n";
+    cout << "Jeg heter Ida, og jeg liker c pluss okuss jeg vet ikke hvor oluss tasten er";
     // keep_window_open(); // Not needed when using project template TDT4102_std_lib_facilities
     return 0;
 }
